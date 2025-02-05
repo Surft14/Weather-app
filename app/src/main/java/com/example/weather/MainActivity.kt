1f29cf49d1dcbf4ac7ac425c08d490d84529455b
Surft14 <mr.darck31@gmail.com>
2025-02-05 09:34:20 +0300

    че то сделал

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -9,14 +9,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.weather.ui.theme.WeatherTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,18 +39,24 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String) {
     Column(modifier = Modifier.fillMaxSize()) {
         Box(
-            modifier = Modifier.fillMaxHeight(0.5f)
-                .background(Color.Red)
+            modifier = Modifier
+                .fillMaxHeight(0.5f)
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
             ){
             Text("Temp in $name")
         }
         Box(
-            modifier = Modifier.fillMaxHeight()
-                .background(Color.Blue)
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentAlignment = Alignment.BottomCenter
         ){
             Button(onClick = {
 
-            }){
+            },
+                modifier = Modifier.fillMaxWidth().padding(5.dp)
+                ){
                 Text("Refresh")
             }
 
